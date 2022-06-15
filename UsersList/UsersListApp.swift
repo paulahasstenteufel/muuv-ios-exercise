042949ad0db84880d62d7411f84f2f37b55ef8b3
Paula Hasstenteufel <paula@hasstenteufel.com>
2022-06-15 17:34:35 -0300

    Setup - project created

---
+++ UsersList/UsersListApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsersListApp.swift
+//  UsersList
+//
+//  Created by Paula Hasstenteufel on 2022-06-15.
+//
+
+import SwiftUI
+
+@main
+struct UsersListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
