ac026b0dcd433a0b43cc025a50d937a078bec277
Paula Hasstenteufel <paula@hasstenteufel.com>
2022-06-17 16:19:06 -0300

    Making all properties Optional to allow fetching empty values

--- Backend/Models/UserDetails.swift
+++ Backend/Models/UserDetails.swift
@@ -12,16 +12,16 @@ struct UserDetails {
     var support: Support
     
     struct Data: Decodable {
-        var id: Decimal
-        var firstName: String
-        var lastName: String
-        var email: String
-        var avatarUrl: URL
+        var id: Decimal?
+        var firstName: String?
+        var lastName: String?
+        var email: String?
+        var avatar: String?
     }
     
     struct Support: Decodable {
-        var url: URL
-        var text: String
+        var url: String?
+        var text: String?
     }
 }
 
@@ -33,7 +33,7 @@ extension UserDetails: Decodable {
         enum DataKeys: String, CodingKey {
             case id
             case email
-            case avatarUrl = "avatar"
+            case avatar
             case firstName = "first_name"
             case lastName = "last_name"
         }
@@ -44,20 +44,23 @@ extension UserDetails: Decodable {
         }
     }
     
+    // Need to define which values need to be successfully fetched
+    // in order to show the screen and then add Error handling adequately.
+    
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         data = try container.decode(Self.Data.self, forKey: .data)
         support = try container.decode(Self.Support.self, forKey: .support)
         
         let dataContainer = try container.nestedContainer(keyedBy: CodingKeys.DataKeys.self, forKey: .data)
-        data.id = try dataContainer.decode(Decimal.self, forKey: .id)
-        data.email = try dataContainer.decode(String.self, forKey: .email)
-        data.avatarUrl = try dataContainer.decode(URL.self, forKey: .avatarUrl)
-        data.firstName = try dataContainer.decode(String.self, forKey: .firstName)
-        data.lastName = try dataContainer.decode(String.self, forKey: .lastName)
+        data.id = try? dataContainer.decode(Decimal.self, forKey: .id)
+        data.email = try? dataContainer.decode(String.self, forKey: .email)
+        data.firstName = try? dataContainer.decode(String.self, forKey: .firstName)
+        data.lastName = try? dataContainer.decode(String.self, forKey: .lastName)
+        data.avatar = try? dataContainer.decode(String.self, forKey: .avatar)
         
         let supportContainer = try container.nestedContainer(keyedBy: CodingKeys.SupportKeys.self, forKey: .support)
-        support.url = try supportContainer.decode(URL.self, forKey: .url)
-        support.text = try supportContainer.decode(String.self, forKey: .text)
+        support.url = try? supportContainer.decode(String.self, forKey: .url)
+        support.text = try? supportContainer.decode(String.self, forKey: .text)
     }
 }
